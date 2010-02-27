94fbad908e06e92acc86c04180159efd6a8e236d
adf <adf@89e66688-367d-0410-b0fd-fb0685a8c2a6>
2010-02-27 14:33:45 +0000

    adding LGPL copyright notice to all source files.

--- gwoptics/src/org/gwoptics/MathUtils/TrigLookup.java
+++ gwoptics/src/org/gwoptics/MathUtils/TrigLookup.java
@@ -1,4 +1,26 @@
+/**
+ *  Copyright notice
+ *  
+ *  This file is part of the Processing library `gwoptics' 
+ *  http://www.gwoptics.org/processing/gwoptics_p5lib/
+ *  
+ *  Copyright (C) 2009 onwards Daniel Brown and Andreas Freise
+ *  
+ *  This library is free software; you can redistribute it and/or modify it under 
+ *  the terms of the GNU Lesser General Public License version 2.1 as published 
+ *  by the Free Software Foundation.
+ *  
+ *  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *  See the GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License along with 
+ *  this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, 
+ *  Suite 330, Boston, MA 02111-1307 USA 
+ */
+
 package org.gwoptics.MathUtils;
+
 /**
  * This class provides access to a much faster version of the cos and sin functions.
  * It generates a table of sin values from 0 to 90 to an accuracy of 0.01 degrees. 

d44c2f1459e4868e158d804c3a031665ff95fa3d
dzb610 <dzb610@89e66688-367d-0410-b0fd-fb0685a8c2a6>
2010-03-01 15:34:52 +0000

    Ammended gwButton methods for using images, also added an example on using the image methods

--- gwoptics/src/org/gwoptics/gui/button/gwButton.java
+++ gwoptics/src/org/gwoptics/gui/button/gwButton.java
@@ -35,7 +35,7 @@ import processing.core.PImage;
  * Simple GButton extension to allow you to alter the border colours without needing
  * to create a GScheme.
  * 
- * Update to 1.3 G4P library. col variable is nolonger being used by GButton so had to
+ * Update to 1.3 G4P library. col variable is no longer being used by GButton so had to
  * update to using the localColor.
  * 
  * Also added functionality to use 3 different button images for normal, over and pressed
@@ -45,11 +45,6 @@ import processing.core.PImage;
  * @since 0.3.7
  */
 public class gwButton extends GButton implements IRenderable {
-
-	public enum gwButtonMode{
-		PLAIN_COLOURS,
-		IMAGES		
-	}
 	
 	private boolean _drawImages;
 	private PImage _imgOver,_imgNorm,_imgPress;
@@ -76,7 +71,7 @@ public class gwButton extends GButton implements IRenderable {
 		_imgPress = imgPressed;
 	}
 	
-	public void setButtonMode(gwButtonMode mode){_drawImages = (mode == gwButtonMode.IMAGES);}
+	public void setUseImages(boolean val){_drawImages = val;}
 	
 	public void setParent(PApplet parent){
 		if(winApp != null)
@@ -85,54 +80,54 @@ public class gwButton extends GButton implements IRenderable {
 		winApp = parent;
 	}
 	
-	public void draw(){
-		if(!visible) return;
-		winApp.pushStyle();
-		//winApp.style(G4P.g4pStyle);
-		Point pos = new Point(0,0);
-		calcAbsPosition(pos);
-		
-		if(_drawImages){
-			switch(status){
-			case 0:
-				winApp.image(_imgNorm,pos.x,pos.y,width,height);
-				break;
-			case 1:
-				winApp.image(_imgOver,pos.x,pos.y,width,height);
-				break;
-			case 2:
-				winApp.image(_imgPress,pos.x,pos.y,width,height);
-				break;
-			}
-		}else{
-			switch(status){
-			case 0:
-				winApp.fill(localColor.btnOff);
-				break;
-			case 1:
-				winApp.fill(localColor.btnOver);
-				break;
-			case 2:
-				winApp.fill(localColor.btnDown);
-				break;
-			}
+public void draw(){
+	if(!visible) return;
+	winApp.pushStyle();
+	//winApp.style(G4P.g4pStyle);
+Point pos = new Point(0,0);
+calcAbsPosition(pos);
 
-			// Draw button rectangle
-			winApp.strokeWeight(1);
-			winApp.stroke(localColor.btnBorder);
-			winApp.rect(pos.x,pos.y,width,height);
-			
-			// Draw image
-			if(bimage[status] != null){
-				winApp.image(bimage[status], pos.x + imgAlignX, pos.y+(height-bimage[status].height)/2);
-			}
-		}	
-		
-		// Draw text
-		winApp.noStroke();
-		winApp.fill(localColor.btnFont);
-		winApp.textFont(localFont, localFont.size);
-		winApp.text(text, pos.x + alignX, pos.y + (height - localFont.size)/2 - PADV, width, height);
-		winApp.popStyle();
+if(_drawImages){
+	switch(status){
+	case 0:
+		winApp.image(_imgNorm,pos.x,pos.y,width,height);
+		break;
+	case 1:
+		winApp.image(_imgOver,pos.x,pos.y,width,height);
+		break;
+	case 2:
+		winApp.image(_imgPress,pos.x,pos.y,width,height);
+		break;
 	}
+}else{
+	switch(status){
+	case 0:
+		winApp.fill(localColor.btnOff);
+		break;
+	case 1:
+		winApp.fill(localColor.btnOver);
+		break;
+	case 2:
+		winApp.fill(localColor.btnDown);
+		break;
+	}
+
+	// Draw button rectangle
+winApp.strokeWeight(1);
+winApp.stroke(localColor.btnBorder);
+winApp.rect(pos.x,pos.y,width,height);
+
+// Draw image
+	if(bimage[status] != null){
+		winApp.image(bimage[status], pos.x + imgAlignX, pos.y+(height-bimage[status].height)/2);
+	}
+}	
+
+// Draw text
+	winApp.noStroke();
+	winApp.fill(localColor.btnFont);
+	winApp.textFont(localFont, localFont.size);
+	winApp.text(text, pos.x + alignX, pos.y + (height - localFont.size)/2 - PADV, width, height);
+	winApp.popStyle();
+}
 }
--- gwoptics/src/org/gwoptics/testing/graphTest.java
+++ gwoptics/src/org/gwoptics/testing/graphTest.java
@@ -1,81 +1,27 @@
 package org.gwoptics.testing;
 
-import java.awt.Image;
-import java.util.ArrayList;
-
-import org.gwoptics.graphics.GWColour;
-import org.gwoptics.graphics.graph2D.Graph2D;
-import org.gwoptics.graphics.graph2D.traces.Blank2DTrace;
-import org.gwoptics.graphics.graph2D.traces.Line2DTrace;
-import org.gwoptics.graphics.graph2D.traces.ILine2DEquation;
-import org.gwoptics.graphics.graph2D.traces.RollingLine2DTrace;
-import org.gwoptics.graphics.graph2D.traces.UpdatingLine2DTrace;
-import org.gwoptics.gui.button.gwButton;
-import org.gwoptics.gui.button.gwButton.gwButtonMode;
+import guicomponents.*;
+import org.gwoptics.gui.button.*;
 
 import processing.core.PApplet;
-import processing.core.PGraphics;
 import processing.core.PImage;
 
 public class graphTest extends PApplet{
-	class Histogram extends Blank2DTrace{
-      	private ArrayList<Float> _data;
-      
-      	public void bindData(ArrayList<Float> data) {_data = data;}
-      
-      	public void TraceDraw(PGraphics canvas) {
-          if(_data != null){
-              
-		      for (int i = 0;i < _data.size(); i++) {		    	  
-	              canvas.fill(50 + i*25);
-	              
-		          float val = _data.get(i);
-		          canvas.rect(i+0.5f,0,1f,val);		          
-		      }
-          }
-      	}
-	}
 	
-	Histogram hTrace;
-	Graph2D g;
-	
+	gwButton btn;
+
 	public void setup(){
-		size(600,500);
-		
-		hTrace  = new Histogram();
-		 
-		g = new Graph2D(this, 400,400, true);
-		g.setAxisColour(220, 220, 220);
-		g.setFontColour(255, 255, 255);
-		
-		g.position.y = 50;
-		g.position.x = 100;
-		
-		g.setYAxisTickSpacing(1f);
-		g.setXAxisTickSpacing(1f);
-		g.setXAxisMinorTicks(1);
-	
-		g.setYAxisMin(-5f);
-		g.setYAxisMax(5f);
-		
-		g.setXAxisMin(0.1f);
-		g.setXAxisMax(5.9f);
-		g.setXAxisLabelAccuracy(0);
-		
-		ArrayList<Float> data = new ArrayList<Float>();
-		data.add(3f);
-		data.add(6f);
-		data.add(-3f);
-		data.add(4f);
-		data.add(2f);
-		
-		hTrace.bindData(data);
-		
-		g.addTrace(hTrace);
+		  size(300,300);
+		    
+		  btn = new gwButton(this,"test",10,10,200,200);
+		  
+		  PImage imgNorm = loadImage("norm.png");
+		  PImage imgOver = loadImage("over.png");
+		  PImage imgPressed = loadImage("pressed.png");
+		  
+		  btn.setUseImages(true);
+		  btn.setImages(imgNorm,imgOver,imgPressed);
 	}
 	
-	public void draw(){
-		background(0);
-		g.draw();
-	}
+	public void draw(){}
 }

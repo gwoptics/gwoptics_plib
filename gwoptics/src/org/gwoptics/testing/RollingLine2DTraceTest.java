7388dcfc004600dd2377e6eee7b43beee4dfcca8
Daniel Brown <ddb@star.sr.bham.ac.uk>
2012-07-04 17:25:58 +0100

    Applied formatting

--- gwoptics/src/org/gwoptics/testing/RollingLine2DTraceTest.java
+++ gwoptics/src/org/gwoptics/testing/RollingLine2DTraceTest.java
@@ -19,11 +19,9 @@ import processing.core.PApplet;
 import processing.core.PGraphics;
 
 
-public class graphTest extends PApplet{
-
-        public static void main(String args[])
-        {
-          
+public class RollingLine2DTraceTest extends PApplet{
+        public static void main(final String[] args) {  
+            PApplet.main( new String[]{RollingLine2DTraceTest.class.getName()} ); 
         }
         
 	class eq implements ILine2DEquation{
@@ -50,29 +48,29 @@ public class graphTest extends PApplet{
 	Graph2D g;
 		
 	public void setup(){
-		size(600,300);
-		
-		r  = new RollingLine2DTrace(new eq() ,10,1.0f);
-		r.setTraceColour(0, 255, 0);
-		
-		r2 = new RollingLine2DTrace(new eq2(),10,1.0f);
-		r2.setTraceColour(255, 0, 0);
-		
-		r3 = new RollingLine2DTrace(new eq3(),10,1.0f);
-		r3.setTraceColour(0, 0, 255);
-		 
-		g = new Graph2D(this, 400, 200, false);
-		g.setYAxisMax(600);
-		g.addTrace(r);
-		g.addTrace(r2);
-		g.addTrace(r3);
-		g.position.y = 50;
-		g.position.x = 100;
-		g.setYAxisTickSpacing(100);
-		g.setXAxisTickSpacing(1f);
-		g.setXAxisLabelAccuracy(2);
-		g.setXAxisMin(0f);
-		g.setXAxisMax(10f);
+          size(600,300);
+
+          r  = new RollingLine2DTrace(new eq() ,100,1.0f);
+          r.setTraceColour(0, 255, 0);
+
+          r2 = new RollingLine2DTrace(new eq2(),100,1.0f);
+          r2.setTraceColour(255, 0, 0);
+
+          r3 = new RollingLine2DTrace(new eq3(),100,1.0f);
+          r3.setTraceColour(0, 0, 255);
+
+          g = new Graph2D(this, 400, 200, false);
+          g.setYAxisMax(600);
+          g.addTrace(r);
+          g.addTrace(r2);
+          g.addTrace(r3);
+          g.position.y = 50;
+          g.position.x = 100;
+          g.setYAxisTickSpacing(100);
+          g.setXAxisTickSpacing(1f);
+          g.setXAxisLabelAccuracy(2);
+          g.setXAxisMin(0f);
+          g.setXAxisMax(10f);
 	}
 
 	public void draw(){
